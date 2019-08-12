399e0909090bf7e98aff17788f15b91ca2af6c21
github username <gregm9999@gmail.com>
2019-08-11 22:52:05 -0700

    Done.

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,16 +1,6 @@
 export function fetchCats() {
   return dispatch => {
     dispatch({ type: "LOADING_CATS" });
-    // return fetch("http://localhost:4000/db/")
-    //   .then(res => {
-    //     return res.json();
-    //   })
-    //   .then(res => {
-    //     console.log(res);
-    //     return res;
-    //   })
-    //   .then(cats => dispatch({ type: "FETCH_CATS", payload: cats.images }))
-    //   .catch(err => console.log(err));
     return fetch("http://localhost:4000/db")
       .then(response => {
         return response.json();
--- src/reducers/cats_reducer.js
+++ src/reducers/cats_reducer.js
@@ -1,8 +1,6 @@
 const catsReducer = (state = { pictures: [], loading: false }, action) => {
   switch (action.type) {
     case "LOADING_CATS":
-      // return { ...state, loading: [...state.loading, true] };
-      // return Object.assign({}, state, { loading: true });
       return { ...state, loading: true };
     case "FETCH_CATS":
       return { ...state, loading: false, pictures: action.payload };
